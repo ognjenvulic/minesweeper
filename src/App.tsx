c25bcd4a104c001acdc56a4631d30bbc15f24adc
Ognjen Vulic <ogi@goowall.io>
2021-12-03 23:26:34 +0100

    Added Saga, Redux-Websocket and Material-Ui libs.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained">Hello World</Button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,47 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import counterReducer from '../features/counter/counterSlice';
+import reduxWebsocket from '@giantmachines/redux-websocket';
+import { connect, send } from '@giantmachines/redux-websocket';
+
+function* helloSaga(): Generator<void, void, unknown> {
+  yield console.log('Hello Sagas!');
+}
+
+const sagaMiddleware = createSagaMiddleware();
+const reduxWebsocketMiddleware = reduxWebsocket({
+  serializer: (payload: any): string | ArrayBuffer | ArrayBufferView | Blob => {
+    console.log(payload);
+    return payload
+  },
+  deserializer: (message: any): any => {
+    console.log(message);
+    return message;
+  },
+  dateSerializer: (date) => date.toISOString(),
+});
+
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,//Fix for reduxWebsocket
+    })
+      .prepend(sagaMiddleware)
+      .prepend(reduxWebsocketMiddleware),
 });
+
+sagaMiddleware.run(helloSaga);
+
+console.log(process.env.WEBSOCKET_ADDR);
+store.dispatch(
+  connect(process.env.WEBSOCKET_ADDR || "wss://hometask.eg1236.com/game1/")
+);
+setTimeout(()=>store.dispatch(send('help')), 1000)
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
